d97c521717f01c4a07d2bc06d19e5af9e11bb2dd
Menma <menma@YUI.>
2023-09-29 23:07:05 +0530

    Modified the navbar design and added new routes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Hero } from './components/Hero'
+import classes from './Home.module.css';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className={classes.container}>
+      <Hero />
+    </div>
   )
 }
 
